46394a382052a26da27fb9e55160300e02b261b5
ktrain0 <59453532+ktrain0@users.noreply.github.com>
2020-05-18 21:28:43 -0700

    First commit

---
+++ src/midi/Main.java
@@ -0,0 +1,15 @@
+package midi;
+
+import jm.music.data.Note;
+import jm.util.Play;
+
+import static jm.constants.Pitches.C4;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+       Note n = new Note(C4,3);
+       Play.midi(n);
+    }
+}
